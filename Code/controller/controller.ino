1ee63b464c29b29b34e1dc5e1ac8939ea0d24e67
Saurabhh-37 <105903203+Saurabhh-37@users.noreply.github.com>
2023-06-30 14:18:44 +0530

    Update controller.ino

--- Code/controller/controller.ino
+++ Code/controller/controller.ino
@@ -11,34 +11,11 @@
 const char* ssid = "ESP32-Access-Point";
 const char* password = "123456789";
 
-/*#include <SPI.h>
-#define BME_SCK 18
-#define BME_MISO 19
-#define BME_MOSI 23
-#define BME_CS 5*/
-
-//Adafruit_BME280 bme; // I2C
-//Adafruit_BME280 bme(BME_CS); // hardware SPI
-//Adafruit_BME280 bme(BME_CS, BME_MOSI, BME_MISO, BME_SCK); // software SPI
-
 /* Assign a unique ID to this sensor at the same time */
 Adafruit_ADXL345_Unified accel = Adafruit_ADXL345_Unified(12345);
 
 // Create AsyncWebServer object on port 80
 AsyncWebServer server(80);
-
-/*String readTemp() {
-  return String(bme.readTemperature());
-  //return String(1.8 * bme.readTemperature() + 32);
-}
-
-String readHumi() {
-  return String(bme.readHumidity());
-}
-
-String readPres() {
-  return String(bme.readPressure() / 100.0F);
-}*/
 
 void displaySensorDetails(void)
 {
@@ -155,26 +132,6 @@ void setup(){
   IPAddress IP = WiFi.softAPIP();
   Serial.print("AP IP address: ");
   Serial.println(IP);
-
-  /*server.on("/temperature", HTTP_GET, [](AsyncWebServerRequest *request){
-    request->send_P(200, "text/plain", readTemp().c_str());
-  });
-  server.on("/humidity", HTTP_GET, [](AsyncWebServerRequest *request){
-    request->send_P(200, "text/plain", readHumi().c_str());
-  });
-  server.on("/pressure", HTTP_GET, [](AsyncWebServerRequest *request){
-    request->send_P(200, "text/plain", readPres().c_str());
-  });
-  
-  bool status;
-
-  // default settings
-  // (you can also pass in a Wire library object like &Wire2)
-  status = bme.begin(0x76);  
-  if (!status) {
-    Serial.println("Could not find a valid BME280 sensor, check wiring!");
-    while (1);
-  }*/
   
   // Start server
   server.begin();
